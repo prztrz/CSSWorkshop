b6a845114f13f22644aa131da5302c74604315b8
Przemysław Trzepiński <prztrz@github.com>
2017-11-02 14:09:09 +0100

    Added comments and updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,68 @@
-# CSSWorkshop
-CSS Workshop
+# Description
+This website is CSS and Javascript workshop to present author's skill and his learning progress.
+
+Whole project has been created with pure JavaScript, HTML and sass-converted css without using any additional libraries.
+
+# Demo
+You can find the demo of the project on: https://prztrz.github.io/Sit-on-Chair/
+
+# Instalation and guidelines
+To install the project simply download it and run the index.html in your web broswer
+
+The project has been made using gulp and gulp-sass tools
+
+#### .scss files
+All scss files listed in /scss folder
+
+* style.scss - main .scss file import all the .scss file created for the project and convert them to css/style.css file
+
+/scss/modules contains: reusable modules for sass
+* \_mixins.scss - contains mixins used in the scss code.
+* \_variables.scss -contains variables used in scss code.
+
+/scss/partials -contains styleshets for particular sections of index.html
+* \_global-classes.scss - contains reusable css selectors
+* \_grid.scss - contains responsive grid for the website
+* \_page-header.scss - contains full stylesheet for header of the website
+* \_section-slider.scss - contains full stylesheet for the section slider
+* \_section-portfolio.scss - contains full stylesheet for the section portfolio
+* \_section-pricelist.scss - contains full stylesheet for the section pricelist
+* \_section-application - contains full stylesheet for the section application
+* \_section-contact.scss - contains full stylesheet for the section contact
+* \_page-footer.scss - contains full stylesheet for footer of the website
+
+#### Scss grid
+
+The grid was created by the author and it bases on 12 columns and 3 window-width breakboints:
+\@tablet breakpoint - \@media (min-width: 767px)
+\@laptop breakpoint - \@media (min-width: 1280px)
+
+columns are <div> element with class col-\*\*-XX
+where \*\* is the specified breakpoint:
+
+sm - for small devices
+md - for medium devices
+lg for large devices
+
+XX (1-12) is the width the column takes on the screen. The sum of all collumns in one row on the screen must be 12.
+
+The grid is scaled up.
+
+#####For example:
+The code:
+<div class="col-sm-12 col-md-6 col-lg-4"></div>
+<div class="col-sm-12 col-md-6 col-lg-4"></div>
+<div class="col-sm-12 col-md-12 col-lg-4"></div>
+
+Represent three columns which will be placed next to each other on large devices. On medium devices first two collumns will be placed next to each other whereas third collumn will be placed in the next row. On small devices all columns will be placed in separate rows.
+
+#### .js files
+All .js files are placed in /js folder:
+
+* menu_app.js - contains scripts for resposnive navigation on the website
+* slider_app.js - contains scripts for slider functionality on the website.
+* pricelist_app - contains scripts for pricelist section on the website
+* application_app - contains scripts for order form and price calculator for the project
 
 # Author
-Przemek
+Przemek Trzepiński
--- js/application_app.js
+++ js/application_app.js
@@ -15,11 +15,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var sum = document.querySelector(".sum > strong");
 
+
+    /**
+     * Object
+     * @name globalPrice
+     * Represents full price of the user's order, based on the  fields titlePrice, colorPrice, patternPrice and transportPrice representinc price of chair model, price of given color, pattern and transport respectively
+     */
+
     var globalPrice = {
         titlePrice: 0,
         colorPrice: 0,
         patternPrice: 0,
         transportPrice: 0,
+        /** @function calculate
+         * Sums every value of @name globalPrice object if type of the value is number
+         * @returns {number} full price of the user's order
+         */
         calculate: function () {
             var values = Object.values(this);
             var result = 0;
@@ -34,9 +45,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 
-    //add click event to all elements with .list_arrow class
+    /**add click event to all elements with .list_arrow class
+    * @listens click
+    */
     for (var i = 0; i < listArrows.length; i++) {
         listArrows[i].addEventListener("click", function() {
+            /**
+             * @this object DOM with .listArrows class
+             */
             var currentList = this.parentElement.querySelector('.list_panel');
             var currentListClasses = currentList.classList;
 
@@ -44,24 +60,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
             currentListClasses.toggle('visible')
 
+            /** add click event to every <li> object which is the child of the same parent as @this
+             * @listens click
+             */
             for (var i = 0; i < currentListItems.length; i++) {
                 currentListItems[i].addEventListener("click", function() {
                     var currentClasses = this.classList;
+
+                    /** Represents every <li> item with the same parent including the clicked item
+                     *  @this object <li>
+                     */
                     var siblingItems = this.parentElement.children;
+
+                    //Label of list the clicked elements is part of.
                     var currentLabel = this.parentElement.parentElement.firstElementChild;
+
+                    //Represents the value of data-price attribute of clicked <li> object
                     var currentPrice = this.dataset.price;
+
+                    //Represents the value of data-category attribute of clicked <li> object
                     var currentCategory = this.parentElement.dataset.category;
 
+                    //If the clicked <li> does not have .selected class
                     if (Array.from(currentClasses).indexOf('selected') === -1) {
+                        //remove selected class from all <li> objects of the same parent
                         for (var i = 0; i < siblingItems.length; i++) {
                             siblingItems[i].classList.remove('selected');
                         }
 
+                        /**add .selected class to @this
+                        *
+                        */
                         currentClasses.add('selected');
+
+                        //Put innerText of selected item to the label and change its color to #000
                         currentLabel.innerText = this.innerText;
                         currentLabel.style.color = '#000';
                     }
 
+                    /** Dependind on value of data-category attribute of clicked list change the corresponding fields in summary pannel and corresponding field of globalPrice object.
+                    *
+                    */
                     switch (currentCategory) {
                         case 'chair-list':
                             summaryTitle.innerText = this.innerHTML;
@@ -82,8 +121,10 @@ document.addEventListener("DOMContentLoaded", function() {
                         break;
                     }
 
-
+                    // put calculated global price of order to sum DOM object
                     sum.innerText = globalPrice.calculate();
+
+                    //drop up the clicked <ul> list
                     currentListClasses.remove('visible');
 
 
@@ -92,21 +133,32 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    /** add click event to check box with transport
+     * @listens click
+     */
     transportCheckBox.addEventListener("click", function() {
+        //represents the value of data-transport-price attribute of the clicked checkbox
         var currentPrice = this.dataset.transportPrice;
 
+        //if the checkbox is checked due to the click
         if (transportCheckBox.checked) {
-
+            //put the value of data-transport-price attribute to the corresponding field in globalPrice object
             globalPrice.transportPrice = parseInt(currentPrice);
+            //put the 'Transport' string to corresponding DOM object in summaryPannel
             transportSpan.innerText = 'Transport';
+            //put the transport price to corresponding DOM object in summaryPannel
             transportPrice.innerText = currentPrice;
 
+        // if the checkbox is unchecked due to the click
         } else {
+            // set transport price to 0
             globalPrice.transportPrice = 0;
+            //remove strings from corresponding DOM objects
             transportSpan.innerText = '';
             transportPrice.innerText = '';
         }
-
+        
+        // put calculated global price of order to sum DOM object
         sum.innerText = globalPrice.calculate();
     });
 });
