6dcc79b5d9637af3ca14b7200bcabb974529a6ae
Przemysław Trzepiński <prztrz@github.com>
2017-11-12 19:14:22 +0100

    slider_app.js code clarification

--- js/slider_app.js
+++ js/slider_app.js
@@ -1,111 +1,122 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //get slider elements
-	var rightBtn = document.getElementById("right-btn");
-	var leftBtn = document.getElementById("left-btn");
-	var slides = document.querySelectorAll(".slide");
+document.addEventListener('DOMContentLoaded', function(e) {
+    class Slider {
+        constructor (collection, switchableClass, classActivate) {
+            this.collection = collection;
+            this.switchableClass = switchableClass;
+            this.classActivate = classActivate;
+        }
 
-    /** @function findCurrentActiveElementIndex
-    * Finds index element with specified class among DOM elements pseudo array
-     *  @param {object} slides Pseudo array with DOM elements
-     * @param {string} className, name of class
-     * @returns {number} index of element with specified class
-     */
-    var findCurrentActiveElementIndex = function (elements, className) {
-        var currentElementIndex = 0;
-        var elementsArray = Array.from(elements)
+        slideRight() {
+            var currentElementIndex = 0;
+            var nextElementIndex = 0
 
-        for (var i = 0; i < elementsArray.length; i++) {
-            if (Array.from(elementsArray[i].classList).indexOf(className) > -1) {
-                currentElementIndex = i;
-                return currentElementIndex;
+            if (this.classActivate) {
+                [].forEach.call(this.collection, (element, i) => {
+                    if (element.classList.contains(this.switchableClass)){
+                        currentElementIndex = i;
+                    }
+                })
+
+            } else {
+                [].forEach.call(this.collection, (element, i) => {
+                    if (!element.classList.contains(this.switchableClass)){
+                        currentElementIndex = i;
+                    }
+                });
+            }
+
+            if (currentElementIndex !== this.collection.length-1) {
+                nextElementIndex = currentElementIndex +1
+            }
+
+
+            if(this.classActivate) {
+                this.collection[currentElementIndex].classList.remove(this.switchableClass);
+                this.collection[nextElementIndex].classList.add(this.switchableClass);
+            } else {
+                this.collection[currentElementIndex].classList.add(this.switchableClass);
+                this.collection[nextElementIndex].classList.remove(this.switchableClass);
+            }
+        }
+
+        slideLeft() {
+            var currentElementIndex = 0;
+            var nextElementIndex = this.collection.length - 1;
+
+            if (this.classActivate) {
+                [].forEach.call(this.collection, (element, i) => {
+                    if (element.classList.contains(this.switchableClass)){
+                        currentElementIndex = i;
+                    }
+                })
+
+            } else {
+                [].forEach.call(this.collection, (element, i) => {
+                    if (!element.classList.contains(this.switchableClass)){
+                        currentElementIndex = i;
+                    }
+                });
+            }
+
+            if (currentElementIndex !== 0) {
+                nextElementIndex = currentElementIndex - 1;
+            }
+
+            if(this.classActivate) {
+                this.collection[currentElementIndex].classList.remove(this.switchableClass);
+                this.collection[nextElementIndex].classList.add(this.switchableClass);
+            } else {
+                this.collection[currentElementIndex].classList.add(this.switchableClass);
+                this.collection[nextElementIndex].classList.remove(this.switchableClass);
+            }
+
+        }
+    }
+
+    class AutoSlider extends Slider {
+        constructor (collection, switchableClass, classActivate, direction, interval) {
+            super(collection, switchableClass, classActivate);
+            this.direction = direction;
+            this.interval = interval;
+        }
+
+        slideRight() {
+            clearInterval(this.intervalId);
+            super.slideRight();
+            this.autoToggle();
+
+        }
+
+        slideLeft() {
+            clearInterval(this.intervalId);
+            super.slideLeft();
+            this.autoToggle();
+        }
+
+        autoToggle() {         
+            var self = this;
+            if (this.direction === 'left'){
+                this.intervalId = setInterval(self.slideLeft.bind(self), self.interval);
+            } else {
+                this.intervalId = setInterval(self.slideRight.bind(self), self.interval)
             }
         }
     }
 
-    /** @function sliderAutoToggler
-    * automatically toggles .active class between elements with .slide class
-     *
-     */
-    var sliderAutoToggler = function () {
-            var currentSlideIndex = findCurrentActiveElementIndex(slides, 'active');
-            var nextSlideIndex = 0;
+    function runSlider() {
+        var slides = document.querySelectorAll('.slide');
+        var leftBtn = document.getElementById('left-btn');
+        var rigthBtn = document.getElementById('right-btn');
 
-            if (currentSlideIndex !== slides.length - 1) {
-                var nextSlideIndex = currentSlideIndex+1
-            }
+        var mainSlider = new AutoSlider(slides, 'active', true, 'right', 3000);
 
-            slides[currentSlideIndex].classList.remove('active');
-            slides[nextSlideIndex].classList.add('active');
+        mainSlider.autoToggle;
+
+        leftBtn.addEventListener('click', mainSlider.slideLeft.bind(mainSlider));
+
+        rigthBtn.addEventListener('click', mainSlider.slideRight.bind(mainSlider));
     }
 
-    /** @function intervalSlideAutoToggler
-    * @async
-    * Runs @function sliderAutoToggler with 3000ms interval
-    */
-	var intervalSlideAutoToggler = setInterval(sliderAutoToggler, 3000);
+    runSlider();
 
-    /**Ads click event on rightBtn DOM object
-        @listens click
-    */
-    rightBtn.addEventListener("click", function() {
-        //turn intervalSlideAutoToggler off
-        clearInterval(intervalSlideAutoToggler);
-
-        /** use @function findCurrentActiveElementIndex to find the index of .slide with .active class amond slides pseudoarray
-        */
-        var currentSlideIndex = findCurrentActiveElementIndex(slides, 'active');
-
-        //set index of the next slide to be shown in slider to 0
-        var nextSlideIndex = 0;
-
-        //if currentSlideIndex is not the highest index in the slides pseudoarray set the nextSlideIndex to currentSlideIndex + 1
-        if (currentSlideIndex !== slides.length - 1) {
-            var nextSlideIndex = currentSlideIndex+1
-        }
-
-        //remove .active class from the currently active slide
-        slides[currentSlideIndex].classList.remove('active');
-
-        //add .active class to the next active slide
-        slides[nextSlideIndex].classList.add('active');
-
-        /** runs @function slideAutoToggler with 3000ms interval
-         * @async
-         */
-        intervalSlideAutoToggler = setInterval(sliderAutoToggler, 3000);
-    });
-
-
-    /**Ads click event on rightBtn DOM object
-        @listens click
-    */
-    leftBtn.addEventListener("click", function() {
-        /** turn @function slideAutoToggler off
-        */
-        clearInterval(intervalSlideAutoToggler);
-
-        /** use @function findCurrentActiveElementIndex to find the index of .slide with .active class amond slides pseudoarray
-        */
-        var currentSlideIndex = findCurrentActiveElementIndex(slides, 'active');
-
-        //set index of the next slide to be shown in slider to the highest index in slides pseudoarray
-        var nextSlideIndex = slides.length-1;
-
-        //if currentSlideIndex is not 0 set the nextSlideIndex to currentSlideIndex - 1
-        if (currentSlideIndex !== 0) {
-            nextSlideIndex = currentSlideIndex -1;
-        }
-
-        //remove .active class from the currently active slide
-        slides[currentSlideIndex].classList.remove('active');
-
-        //add .active class to the next active slide
-        slides[nextSlideIndex].classList.add('active');
-
-        /** runs @function slideAutoToggler with 3000ms interval
-         * @async
-         */
-        intervalSlideAutoToggler = setInterval(sliderAutoToggler, 3000);
-
-    });
 });
